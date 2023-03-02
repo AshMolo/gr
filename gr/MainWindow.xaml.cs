4735036e11877ebc0ac595988ece3696d47fd597
user <user@526-1>
2023-03-02 11:34:33 +0400

    Добавьте файлы проекта.

---
+++ gr/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Crypto.AES;
+
+namespace gr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_interactive_Click(object sender, RoutedEventArgs e)
+        {
+            AES aes = new AES(tb_key.Text);
+            if(rb_crypt.IsChecked.Value)
+            {
+                tb_result.Text = aes.Encrypt(tb_content.Text);
+            }
+           else if (rb_decrypt.IsChecked.Value)
+            {
+                tb_result.Text = aes.Decrypt(tb_content.Text);
+            }
+        }
+    }
+}
